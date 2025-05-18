3251ea421db220d79eb845c39724b23e1cb1eeb2
Svaella <svaellah1@upao.edu.pe>
2025-05-18 15:11:45 -0500

    fix: descargar preprocessor

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,14 @@
 from pydantic_settings import BaseSettings
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent  # ✅ /app
-MODEL_DIR = BASE_DIR / "model"  # ✅ /app/model
+# ✅ BASE_DIR = carpeta /app (donde está main.py)
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "Sistema de Valoración de Riesgo de Hipertensión Arterial"
-    MODEL_PATH: Path = MODEL_DIR
-    
+    MODEL_PATH: Path = BASE_DIR / "model"  # → /app/model
+
     class Config:
         case_sensitive = True
 
